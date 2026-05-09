ae581c60ce927da337860ffd3f7ec18896ae6839
guts-my <keanhong639@gmail.com>
2026-05-09 16:58:59 +0800

    added new api request

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,23 @@
 import allure
+import requests
 
-@allure.feature("Math Module")
-@allure.story("Basic arithmetic")
-def test_addition():
-    with allure.step("Check addition logic"):
-        assert 2 + 2 == 4
+def attach_log(name, content):
+    allure.attach(
+        content,
+        name=name,
+        attachment_type=allure.attachment_type.TEXT
+    )
 
-        #comment
+# https://pokeapi.co/api/v2/pokemon/pikachu
+@allure.feature("PokeAPI")
+def test_pokemon_type_validation():
+    with allure.step("Get Pikachu data"):
+        response = requests.get("https://pokeapi.co/api/v2/pokemon/pikachu")
+        data = response.jpson()
+    
+    with allure.step("Verify Pikachu is Electric type"):
+        types = [t['type']['name'] for t in data['types']]
+        assert "electric" in types, f"Expected 'electric' in {types}"
+    
+    with allure.step("Verify name is pikachu"):
+        assert data['name'] == "pikachu"
